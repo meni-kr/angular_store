20e65553b9a92b8c6ccfaee50ad0e995090ce4e1
meni-kr <meni.kramer@gmail.com>
2024-11-07 01:33:46 +0200

    Add cart icon with dropdown to view the items

--- src/app/cmps/app-header/app-header.component.ts
+++ src/app/cmps/app-header/app-header.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatBadgeModule,
+    MatButtonModule,
+    MatMenuModule,
+    CommonModule,
+    RouterLink
+  ],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.css'
 })
