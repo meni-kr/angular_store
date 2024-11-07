a7271dbfed3850d92bdb93dbf37e421c1547ee4a
meni-kr <meni.kramer@gmail.com>
2024-11-07 11:41:18 +0200

    Create filter cmp

---
+++ src/app/cmps/filter/filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './filter.component.html',
+  styleUrl: './filter.component.css'
+})
+export class FilterComponent {
+
+}
