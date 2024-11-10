f33ceb6172e76c83b518cfc68ff248ac6c7079de
meni-kr <meni.kramer@gmail.com>
2024-11-11 00:31:10 +0200

    Add cart cmp and route for it

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cart',
+  standalone: true,
+  imports: [],
+  templateUrl: './cart.component.html',
+  styleUrl: './cart.component.css'
+})
+export class CartComponent {
+
+}
