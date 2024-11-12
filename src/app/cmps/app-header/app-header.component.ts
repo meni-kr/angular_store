69324df71b56730faa5eb5e96835f1a0e03280dd
meni-kr <meni.kramer@gmail.com>
2024-11-12 12:25:44 +0200

    Style the cart cmp

--- src/app/cmps/app-header/app-header.component.ts
+++ src/app/cmps/app-header/app-header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { MaterialModule } from '../../models/material.module';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    MaterialModule
+    MaterialModule,
+    RouterLink
   ],
   templateUrl: './app-header.component.html',
   styleUrl: './app-header.component.css'
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,38 +1,57 @@
 import { Component, OnInit } from '@angular/core';
 import { MaterialModule } from '../../models/material.module';
 import { Cart, CartItem } from '../../models/cart.model';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'cart',
   standalone: true,
-  imports: [MaterialModule],
+  imports: [MaterialModule, RouterLink],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css'
 })
-export class CartComponent implements OnInit{
-  
-  cart:Cart ={items:[
-    {
-      product:'https://placehold.co/150',
-      name:'snickers',
-      price:150,
-      quantity:1,
-      _id:1
-    }
-  ]}
-  
-  dataSource: CartItem[]=[]
-  displayedColumns:string[] = [
+export class CartComponent implements OnInit {
+
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://placehold.co/150',
+        name: 'snickers',
+        price: 150,
+        quantity: 1,
+        _id: 1
+      },
+      {
+        product: 'https://placehold.co/150',
+        name: 'snickers',
+        price: 150,
+        quantity: 1,
+        _id: 2
+      },
+    ]
+  }
+
+  dataSource: CartItem[] = []
+  displayedColumns: string[] = [
     'product',
     'name',
     'price',
     'quantity',
     'total',
-    'action'
+    'action',
   ]
-  
+  total: number = 0
+
   ngOnInit(): void {
- this.dataSource = this.cart.items
-}
+    this.dataSource = this.cart.items
+    this.getTotal()
+
+  }
+
+  getTotal() {
+    this.total = this.cart.items
+      .map(item => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0)
+  }
 
 }
