6ed69f08bacaffb577aa3faefe9df128c38988d5
meni-kr <meni.kramer@gmail.com>
2024-11-12 14:15:24 +0200

    Add product model and add on add to cart function and add cart service

--- src/app/cmps/product-preview/product-preview.component.ts
+++ src/app/cmps/product-preview/product-preview.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input, input } from '@angular/core';
+import { Component, Input, input, output } from '@angular/core';
 import { MaterialModule } from '../../models/material.module';
+import { ProductModel } from '../../models/product.model';
 
 @Component({
   selector: 'product-preview',
@@ -11,5 +12,20 @@ import { MaterialModule } from '../../models/material.module';
 export class ProductPreviewComponent {
   fullWidthMode = input<boolean>(false)
   // @Input() fullWidthMode = false;
+
+  addToCart = output<ProductModel>()
+
+  product: ProductModel | undefined ={
+    _id: 1,
+    title: 'Nike Air Max 90',
+    price: 120,
+    category: 'shoes',
+    description: 'Nike Air Max 90 is a high-performance running shoe. It features a breathable upper and a lightweight design.',
+    image: 'https://placehold.co/150'
+  }
+
+  onAddToCart() {
+   this.addToCart.emit(this.product!)
+  }
 
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,6 +3,7 @@ import { MaterialModule } from '../../models/material.module';
 import { ProductsHeaderComponent } from "../../cmps/products-header/products-header.component";
 import { ProductFilterComponent } from "../../cmps/product-filter/product-filter.component";
 import { ProductPreviewComponent } from '../../cmps/product-preview/product-preview.component';
+import { ProductModel } from '../../models/product.model';
 
 
 const ROWS_HEIGHT:{[id:number]:number} ={1:400,3:335,4:350}
@@ -20,19 +21,23 @@ const ROWS_HEIGHT:{[id:number]:number} ={1:400,3:335,4:350}
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  
   cols = 3
   rowHeight = ROWS_HEIGHT[this.cols]
   category: string |undefined
-
+  
   onColumnsCountChange(colsNum: number) {
     this.cols = colsNum
     this.rowHeight = ROWS_HEIGHT[this.cols]
   }
-
+  
   onShowCategory(newCategory: string): void {
     this.category = newCategory
-
+    
+  
+  }
+  onAddToCart(): void {
+    
   }
 
 }
